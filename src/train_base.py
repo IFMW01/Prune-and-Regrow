8d190f360c89d9fbbce24efbc0b63ba743143a28
Israel Francis Mason-Williams <55697240+IFMW01@users.noreply.github.com>
2024-04-09 11:47:20 +0100

    Futher updates

--- src/train_base.py
+++ src/train_base.py
@@ -20,7 +20,6 @@ def main(config):
     pipeline = config.get("pipeline", None)
     architecture = config.get("architecture", None)
     n_epochs = config.get("n_epochs", None)
-    training = config.get("training", None)
     seeds = config.get("seeds", None)
     n_classes = config.get("n_classes", None)
     n_inputs = config.get("n_inputs", None)
@@ -30,29 +29,25 @@ def main(config):
     print(f"Pipeline: {pipeline}")
     print(f"Architecture: {architecture}")
     print(f"Number of epochs: {n_epochs}")
-    print(f"Training: {training}")
     print(f"Seeds: {seeds}")
 
     device = utils.get_device()
-            
-    if training == 'Base':
-        save_dir = "TRAIN"
+    save_dir = 'TRAIN'
+    utils.create_dir(save_dir)
+    save_dir = os.path.join(save_dir, f"{dataset_pointer}")
+    utils.create_dir(save_dir)
+    save_dir = os.path.join(save_dir, f"{architecture}")
+    utils.create_dir(save_dir)
+    train_loader,valid_loader,test_loader = ld.load_datasets(dataset_pointer,pipeline)
+    for seed in seeds:
+        save_dir = os.path.join(f"TRAIN/{dataset_pointer}/{architecture}")
+        utils.set_seed(seed)
+        model,optimizer, scheduler,criterion = utils.initialise_model(architecture,n_inputs,n_classes,device)
+        save_dir = os.path.join(save_dir, f"{seed}")
         utils.create_dir(save_dir)
-        save_dir = os.path.join(save_dir, f"{dataset_pointer}")
-        utils.create_dir(save_dir)
-        save_dir = os.path.join(save_dir, f"{architecture}")
-        utils.create_dir(save_dir)
-        train_loader,valid_loader,test_loader = ld.load_datasets(dataset_pointer,pipeline)
-        for i in range(len(seeds)):
-            save_dir = os.path.join(f"{training}_{dataset_pointer}", f"{architecture}")
-            seed = seeds[i]
-            utils.set_seed(seed)
-            model,optimizer, scheduler,criterion = utils.initialise_model(architecture,n_inputs,n_classes,device)
-            save_dir = os.path.join(save_dir, f"{seed}")
-            utils.create_dir(save_dir)
-            print(save_dir)
-            save_path = save_dir + '/'
-            create_base_model(model,optimizer,criterion,save_path,device, n_epochs, seed,train_loader,valid_loader,test_loader)
+        print(save_dir)
+        save_path = save_dir + '/'
+        create_base_model(model,optimizer,criterion,save_path,device, n_epochs, seed,train_loader,valid_loader,test_loader)
     print("FIN")
 
 if __name__ == "__main__":
--- src/unlearn.py
+++ src/unlearn.py
@@ -6,6 +6,7 @@ import os
 import training as tr
 import load_datasets as ld
 import membership_inference as mi
+import glob
 import utils 
 
 def main(config):
@@ -25,28 +26,40 @@ def main(config):
     print(f"Dataset pointer: {dataset_pointer}")
     print(f"Architecture: {architecture}")
     print(f"Number of retrain epochs: {n_epochs}")
-
     print(f"Seeds: {seeds}")
+    print(f"Number of impair epochs: {n_impair}")
+    print(f"Number of repair epochs: {n_repair}")
+    print(f"Number of fine tuning epochs: {n_fine_tine}")
 
     device = utils.get_device()
-            
-    if training == 'Base':
-        save_dir = f"{training}_{dataset_pointer}"
-        utils.create_dir(save_dir)
-        save_dir = os.path.join(save_dir, f"{architecture}")
-        utils.create_dir(save_dir)
-        train_loader,valid_loader,test_loader = ld.load_datasets(dataset_pointer,pipeline)
-        for i in range(len(seeds)):
-            save_dir = os.path.join(f"{training}_{dataset_pointer}", f"{architecture}")
-            seed = seeds[i]
-            utils.set_seed(seed)
-            model,optimizer, scheduler,criterion = utils.initialise_model(architecture,n_inputs,n_classes,device)
-            save_dir = os.path.join(save_dir, f"{seed}")
+    model_dir = f'TRAIN/{dataset_pointer}/{architecture}'
+    if not os.IsADirectoryError(model_dir):
+        print(f"There are no models with this {architecture} for this {dataset_pointer} in the TRAIN directory. Please train relevant models")
+        return
+    else:
+        for seed in seeds:
+            model_dir = f'TRAIN/{dataset_pointer}/{architecture}/{seed}'
+            print(f"Acessing trained model on seed: {seed}")
+            model_path = glob.glob(os.path.join(model_dir, '*.pth'))
+
+
+        if training == 'Base':
+            save_dir = f"{training}_{dataset_pointer}"
             utils.create_dir(save_dir)
-            print(save_dir)
-            save_path = save_dir + '/'
-            create_base_model(model,optimizer,criterion,save_path,device, n_epochs, seed,train_loader,valid_loader,test_loader)
-    print("FIN")
+            save_dir = os.path.join(save_dir, f"{architecture}")
+            utils.create_dir(save_dir)
+            train_loader,valid_loader,test_loader = ld.load_datasets(dataset_pointer,pipeline)
+            for i in range(len(seeds)):
+                save_dir = os.path.join(f"{training}_{dataset_pointer}", f"{architecture}")
+                seed = seeds[i]
+                utils.set_seed(seed)
+                model,optimizer, scheduler,criterion = utils.initialise_model(architecture,n_inputs,n_classes,device)
+                save_dir = os.path.join(save_dir, f"{seed}")
+                utils.create_dir(save_dir)
+                print(save_dir)
+                save_path = save_dir + '/'
+                create_base_model(model,optimizer,criterion,save_path,device, n_epochs, seed,train_loader,valid_loader,test_loader)
+        print("FIN")
 
 if __name__ == "__main__":
     with open("./config.json", "r") as f:
