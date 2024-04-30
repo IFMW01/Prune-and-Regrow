5be9fb96b2c4241ac20a804cc8b7702b8ab49d9d
Israel Francis Mason-Williams <55697240+IFMW01@users.noreply.github.com>
2024-04-30 10:08:49 +0100

    Using official attack model implementation

--- src/attack.py
+++ src/attack.py
@@ -9,8 +9,11 @@ from pytorch_tabnet.tab_model import TabNetClassifier
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from torch.utils.data import DataLoader,TensorDataset
+import Trainer
 
-def attack_models(num_models,x_train,y_train,x_test,y_test,attack_model,save_dir,device):
+
+def attack_models_old(num_models,x_train,y_train,x_test,y_test,attack_model,save_dir,device):
   for i in range(num_models):
     utils.set_seed(i)
     if attack_model == 'xgb':
@@ -60,6 +63,19 @@ def attack_models(num_models,x_train,y_train,x_test,y_test,attack_model,save_dir
     print(f"ATTACK MODEL: {save_name} STATS")
     modelstats(model,x_train,x_test,y_train,y_test)
 
+def attack_model(num_models,train_loader,test_loader,n_classes,save_dir,device):
+
+  for i in range(num_models):
+    utils.set_seed(i)
+    train = Trainer(model, train_loader, train_loader, test_loader, optimizer, criterion, device, 50,n_classes,i)
+
+    save_name = f'tabnet_model_{i}.pth'
+    save_path = f"{save_dir}/{save_name}"
+    torch.save(model, save_path)
+       
+    print(f"ATTACK MODEL: {save_name} STATS")
+    modelstats(model,x_train,x_test,y_train,y_test)
+
 
 def modelstats(model,x_train,x_test,y_train,y_test):
   y_pred_train = model.predict(x_train)
@@ -104,45 +120,55 @@ def create_mia_datasets(data_directory):
   balanced_df = balanced_df.drop(['label'], axis=1)
 
   x_train,x_test,y_train,y_test= train_test_split(balanced_df,df_lables, test_size=0.2, random_state=42,shuffle=True)
-  print(x_train)
-  print(y_train)
+
   return x_train,y_train,x_test,y_test
 
+def create_mia_loader(x_train,y_train,x_test,y_test):
+  train = TensorDataset(x_train, y_train)
+  test = TensorDataset(x_test, y_test)
+  train_loader = DataLoader(train, batch_size=264, shuffle=True)
+  test_loader = DataLoader(test, batch_size=264, shuffle=False)
+  return train_loader,test_loader
+
 def main(config_attack,config_base):
-    dataset_pointer = config_base.get("dataset_pointer", None)
-    architecture = config_base.get("architecture", None)
-    n_attack_models = config_attack.get("n_attack_models", None)
+  dataset_pointer = config_base.get("dataset_pointer", None)
+  architecture = config_base.get("architecture", None)
+  n_attack_models = config_attack.get("n_attack_models", None)
 
-    device = utils.get_device()
-    dataset_dir = f'TRAIN/{dataset_pointer}/{architecture}/MIA'
-    if not os.path.exists(dataset_dir):
-        print(f"There are no models with this {architecture} for this {dataset_pointer} in the MIA directory. Please train relevant models")
-        return
-    logit_dir = dataset_dir + '/Logits'
-    softmax_dir = dataset_dir + '/Softmax'
-    logit_attack = logit_dir +"/Attack"
-    softmax_attack = softmax_dir +"/Attack"
-    utils.create_dir(logit_attack)
-    utils.create_dir(softmax_attack)
+  device = utils.get_device()
+  dataset_dir = f'TRAIN/{dataset_pointer}/{architecture}/MIA'
+  if not os.path.exists(dataset_dir):
+      print(f"There are no models with this {architecture} for this {dataset_pointer} in the MIA directory. Please train relevant models")
+      return
+  logit_dir = dataset_dir + '/Logits'
+  softmax_dir = dataset_dir + '/Softmax'
+  logit_attack = logit_dir +"/Attack"
+  softmax_attack = softmax_dir +"/Attack"
+  utils.create_dir(logit_attack)
+  utils.create_dir(softmax_attack)
 
-    x_train_logits,y_train_logits,x_test_logits,y_test_logits = create_mia_datasets(logit_dir)
-    x_train_loss,y_train_loss,x_test_loss,y_test_loss = create_mia_datasets(softmax_dir)
+  x_train_logits,y_train_logits,x_test_logits,y_test_logits = create_mia_datasets(logit_dir)
+  x_train_loss,y_train_loss,x_test_loss,y_test_loss = create_mia_datasets(softmax_dir)
+  train_logits,test_logits = create_mia_loader(x_train_logits,y_train_logits,x_test_logits,y_test_logits)
+  train_loss,test_loss = create_mia_loader(x_train_loss,y_train_loss,x_test_loss,y_test_loss)
 
-    print("Logit Attack Models")
-    # attack_model = 'xgb'
-    # attack_models(n_attack_models,x_train_logits,y_train_logits,x_test_logits,y_test_logits,attack_model,logit_attack,device)
-    attack_model = 'tabnet'
-    attack_models(n_attack_models,x_train_logits,y_train_logits,x_test_logits,y_test_logits,attack_model,logit_attack,device)
-    print("Loss Attack Models")
-    # attack_model = 'xgb'
-    # attack_models(n_attack_models,x_train_loss,y_train_loss,x_test_loss,y_test_loss,attack_model,softmax_attack,device)
-    attack_model = 'tabnet'   
-    attack_models(n_attack_models,x_train_loss,y_train_loss,x_test_loss,y_test_loss,attack_model,softmax_attack,device)
-    print("FIN")
+
+
+  print("Logit Attack Models")
+  # attack_model = 'xgb'
+  # attack_models(n_attack_models,x_train_logits,y_train_logits,x_test_logits,y_test_logits,attack_model,logit_attack,device)
+  attack_model = 'tabnet'
+  # attack_models(n_attack_models,x_train_logits,y_train_logits,x_test_logits,y_test_logits,attack_model,logit_attack,device)
+  print("Loss Attack Models")
+  # attack_model = 'xgb'
+  # attack_models(n_attack_models,x_train_loss,y_train_loss,x_test_loss,y_test_loss,attack_model,softmax_attack,device)
+  attack_model = 'tabnet'   
+  attack_models(n_attack_models,x_train_loss,y_train_loss,x_test_loss,y_test_loss,attack_model,softmax_attack,device)
+  print("FIN")
 
 if __name__ == "__main__":
-    with open("./configs/base_config.json","r") as b:
-        config_base = json.load(b)    
-    with open("./configs/attack_config.json", "r") as a:
-        config_attack = json.load(a)
-    main(config_attack,config_base)
+  with open("./configs/base_config.json","r") as b:
+      config_base = json.load(b)    
+  with open("./configs/attack_config.json", "r") as a:
+      config_attack = json.load(a)
+  main(config_attack,config_base)
---
+++ src/models/attack_model.py
@@ -0,0 +1,55 @@
+# https://github.com/teobaluta/etio/blob/main/attack/attack_models.py
+
+#  shokri_shadow_model_attack as attack models trained using the data we obtain from the shadow models
+
+import json
+import sys
+from torch.nn import init
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SoftmaxModel(nn.Module):
+    def __init__(self, n_in, n_out):
+        super().__init__()
+        self.fc1 = nn.Linear(n_in, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, n_out)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.tanh(x)
+        x = self.fc2(x)
+        x = torch.tanh(x)
+        x = self.fc3(x)
+        y = x
+        return y
+
+def softmax_net(n_inputs):
+    # width=int(config["width"])
+    net = SoftmaxModel(n_in=n_inputs, n_out = 2)
+    return net
+
+class SimpleNet(nn.Module):
+
+    def __init__(self, n_inputs, hidden_layers, out_dim=2):
+        super(SimpleNet, self).__init__()
+        self.layers = [nn.Linear(n_inputs, hidden_layers), nn.ReLU(inplace=True)]
+        for i in range(1,len(hidden_layers)):
+            self.layers.append(nn.Linear(hidden_layers[i-1], hidden_layers[i]))
+            self.layers.append(nn.ReLU(inplace=True))
+
+        self.net = nn.Sequential(*self.layers,
+            nn.Linear(hidden_layers[-1], out_dim)
+        )
+
+
+    def forward(self, x):
+        x = self.net(x)
+        return x
+
+def simplenet(n_inputs,hidden_layers):
+    # width=int(config["width"])
+    net = SimpleNet(hidden_layers, n_inputs)
+    return net
