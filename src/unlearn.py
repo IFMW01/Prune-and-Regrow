5e669c0c5711f2a1be3073f3828dce605dd929b3
Israel Francis Mason-Williams <55697240+IFMW01@users.noreply.github.com>
2024-05-21 02:20:30 +0100

    Update unlearn.py

--- src/unlearn.py
+++ src/unlearn.py
@@ -56,7 +56,7 @@ def unlearning_process(remain_loader,remain_eval_loader,forget_loader,forget_eva
         print(f'\nSeed: {seed}')
         results_dict[seed] = {}
         model_dir = f'Results/{dataset_pointer}/{architecture}/{seed}'
-        save_dir = f"Results/{dataset_pointer}/{architecture}/UNLEARN/{tag}/{forget_amount}/{seed}/OMP"
+        save_dir = f"Results/{dataset_pointer}/{architecture}/UNLEARN/{tag}/{forget_amount}/{seed}/OMP/"
         utils.create_dir(save_dir)
         print(f"Acessing trained model on seed: {seed}")
         model_path = glob.glob(os.path.join(model_dir,'*.pth'))
@@ -68,7 +68,7 @@ def unlearning_process(remain_loader,remain_eval_loader,forget_loader,forget_eva
         logit_distributions(orginal_model,remain_eval_loader,forget_eval_loader,test_loader,device,save_dir,'orginal_model_loss')
 
         results_dict[seed]["Naive Unlearning"] = {}
-        if not os.path.isfile(f"{save_dir}/Naive.pth"):
+        if not os.path.isfile(f"{save_dir}Naive.pth"):
             naive_model,results_dict[seed]["Naive Unlearning"] = um.naive_unlearning(architecture,n_inputs,n_classes,device,remain_loader,remain_eval_loader,test_loader,forget_loader,forget_eval_loader,n_epochs,results_dict[seed]["Naive Unlearning"],seed)
             logit_distributions(naive_model,remain_eval_loader,forget_eval_loader,test_loader,device,save_dir,'naive_model_loss')
             torch.save(naive_model,f"{save_dir}Naive.pth")
@@ -81,7 +81,7 @@ def unlearning_process(remain_loader,remain_eval_loader,forget_loader,forget_eva
             results_dict[seed]["Naive Unlearning"]["JS divergance"] = (unlearn_metrics.JS_divergence(naive_model,naive_model,forget_eval_loader,device)) 
             loss_results = unlearn_metrics.mia_efficacy(naive_model,forget_loader,n_classes,device)
             results_dict[seed]["Naive Unlearning"]["Loss MIA"] =   loss_results 
-            with open(f"{save_dir}/Niave.json",'w') as f:
+            with open(f"{save_dir}Niave.json",'w') as f:
                 json.dump(results_dict,f)
         else:
             naive_model,optimizer,criterion = um.load_model(f"{save_dir}Naive.pth",architecture,0.01,device)
@@ -175,7 +175,7 @@ def unlearning_process(remain_loader,remain_eval_loader,forget_loader,forget_eva
         print(f'All unlearning methods applied for seed: {seed}.\n{results_dict}')
 
                 
-    with open(f"{save_dir}/unlearning_omp_results.json",'w') as f:
+    with open(f"{save_dir}unlearning_omp_results.json",'w') as f:
         json.dump(results_dict,f)
     
 
